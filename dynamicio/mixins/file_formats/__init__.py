cb3a8715e156a10c490af8be55f874581555dd04
Ilja Orlovs <ilja.orlovs@vortexa.com>
2022-12-02 13:49:10 +0000

    Revert "refactor: created file format readers and writers"
    This reverts commit 6ed538ceeb14d1f0fe942f21af9672bdceede131.

--- dynamicio/mixins/file_formats/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Helper modules implementing support of various file formats sent over various media (local storage, file objects, etc)."""
-
-from .generic_pandas import CSV, JSON, Parquet
-from .hdf import HDF
