198ed33c8b1d94a4cedfe9ed2d45a972a7cd7f82
Ilja Orlovs <ilja.orlovs@vortexa.com>
2022-12-16 11:01:05 +0000

    test: Restored the missing fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -308,8 +308,13 @@ def expected_s3_parquet_df():
 
 
 @pytest.fixture(scope="class")
-def expected_s3_hdf_df():
-    return pd.read_hdf(f"{constants.TEST_RESOURCES}/data/input/some_hdf_to_read.h5")
+def expected_s3_hdf_file_path():
+    return f"{constants.TEST_RESOURCES}/data/input/some_hdf_to_read.h5"
+
+
+@pytest.fixture(scope="class")
+def expected_s3_hdf_df(expected_s3_hdf_file_path):  # pylint: disable=redefined-outer-name
+    return pd.read_hdf(expected_s3_hdf_file_path)
 
 
 @pytest.fixture(scope="class")
