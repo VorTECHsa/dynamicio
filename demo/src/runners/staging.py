609b459c61f6ad7c15baf79fbc0765ac33366359
Christos Hadjinikolis <christos.hadjinikolis@vortexa.com>
2023-02-25 17:58:26 +0000

    wip

--- demo/src/runners/staging.py
+++ demo/src/runners/staging.py
@@ -17,8 +17,13 @@ def main() -> None:
     # LOAD DATA
     logger.info("Loading data from live sources...")
 
-    bar_df = InputIO(resource_definition=staging_input_config.get(source_key="BAR"), schema=Bar, apply_schema_validations=True, log_schema_metrics=True).read()
-    foo_df = InputIO(resource_definition=staging_input_config.get(source_key="FOO"), scheam=Foo, apply_schema_validations=True, log_schema_metrics=True).read()
+    bar_df = UnifiedIO.read(
+        resource_definition=staging_input_config.get(source_key="BAR"),
+        schema=Bar,
+        apply_schema_validations=True,
+        log_schema_metrics=True
+    )
+    foo_df = InputIO(resource_definition=staging_input_config.get(source_key="FOO"), schema=Foo, apply_schema_validations=True, log_schema_metrics=True).read()
 
     logger.info("Data successfully loaded from live sources...")
 
@@ -31,6 +36,15 @@ def main() -> None:
 
     # SINK DATA
     logger.info("Begin sinking data to staging area:")
-    StagedFooIO(resource_definition=staging_output_config.get(source_key="STAGED_FOO"), **constants.TO_PARQUET_KWARGS).write(foo_df)
-    StagedBarIO(resource_definition=staging_output_config.get(source_key="STAGED_BAR")).write(bar_df)
+    UnifiedIO(resource_definition=staging_output_config.get(source_key="STAGED_FOO"), **constants.TO_PARQUET_KWARGS).write(foo_df)
+    UnifiedIO(resource_definition=staging_output_config.get(source_key="STAGED_BAR")).write(bar_df)
     logger.info("Data staging is complete...")
+
+
+
+ # 1. Use UnifiedIO as IO class for loading writing data
+ # 2. Introduce pandera schema as extra field in UnifiedIO parameters
+ # 3. Write script to generate pandera schema from dataframe (extra propose logging and validations too)
+ # 4. Write script to migrate from yamls schemas to pandera schemas
+ # 5. Write validations that are not available through pandera standard validations but we use after we identify what is missing!
+ # 6. Confirm everything works
