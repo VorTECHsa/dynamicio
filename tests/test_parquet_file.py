9cfb58f58f59aaf1877f284c5f2eed4fa488604d
Joscha Gutjahr <joscha.gutjahr@vortexa.com>
2023-04-04 18:39:54 +0100

    pylint

--- tests/test_parquet_file.py
+++ tests/test_parquet_file.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-module-docstring, missing-class-docstring, missing-function-docstring, R0801, W0621
+# pylint: disable=missing-module-docstring, missing-class-docstring, missing-function-docstring, R0801, W0621, W0212
 import pandas as pd
 import pytest
 
