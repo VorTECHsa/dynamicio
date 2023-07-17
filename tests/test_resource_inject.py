afc223d63544e0cf58b17b790970b0248c524e57
Joscha Gutjahr <joscha.gutjahr@vortexa.com>
2023-07-17 12:03:11 +0100

    Fix test writing to files

--- tests/test_resource_inject.py
+++ tests/test_resource_inject.py
@@ -39,8 +39,8 @@ def test_file_resource_inject_fail(file_resource, injectable_string, failing_inj
         file_resource.write(test_df)
 
 
-def test_file_resource_inject_success(file_resource, injectable_string, passing_injections, test_df):
-    file_resource.path = Path(injectable_string)
+def test_file_resource_inject_success(file_resource, injectable_string, passing_injections, test_df, tmpdir):
+    file_resource.path = Path(tmpdir / injectable_string)
     file_resource = file_resource.inject(**passing_injections)
     file_resource.write(test_df)
     file_resource.read()
