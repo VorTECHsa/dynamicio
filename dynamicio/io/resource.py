dacd474f0a258edcbbcdff8c243fea3ad833c1e6
Joscha Gutjahr <joscha.gutjahr@vortexa.com>
2023-09-05 10:08:56 +0100

    Add base resource and serde classes

---
+++ dynamicio/io/resource.py
@@ -0,0 +1,95 @@
+from abc import abstractmethod
+from copy import deepcopy
+from pathlib import Path
+from typing import Callable, List, Optional, Type
+
+import pandas as pd
+from pandera import SchemaModel
+from pydantic import BaseModel
+from uhura import Readable, Writable
+
+from dynamicio.io.serde import BaseSerde, PickleSerde
+
+
+def create_schema_validator(schema) -> Callable[[pd.DataFrame], pd.DataFrame]:
+    def validate_schema(df: pd.DataFrame):
+        return schema.validate(df)
+
+    return validate_schema
+
+
+class BaseResource(BaseModel, Readable[pd.DataFrame], Writable[pd.DataFrame]):  # Cacheable kaputt, ABC kaputt
+    """Base class for all resources.
+
+    :injectables: List of attributes that can be injected with format strings.
+    :test_path (optional): Path to the test data. If set, the resource will be read from and written to this path.
+    :pa_schema (optional): Pandera schema to validate the resource. If set, the resource will be validated before writing and after reading.
+    """
+
+    injectables: List[str]
+    test_path: Optional[Path] = None
+    pa_schema: Optional[Type[SchemaModel]] = None
+
+    def inject(self, **kwargs) -> "BaseResource":
+        # copy object
+        clone = deepcopy(self)
+        for injectable in self.injectables:
+            # inject attributes
+            value = getattr(clone, injectable)
+            if isinstance(value, str) or isinstance(value, Path):
+                formatted_str = str(value).format(**kwargs)
+                setattr(clone, injectable, formatted_str)
+            else:
+                raise ValueError(f"Cannot inject {injectable} of type {type(value)}")
+
+            # inject test path
+            if self.test_path is not None:
+                clone.test_path = str(clone.test_path).format(**kwargs)
+        return clone
+
+    @abstractmethod
+    def _read(self) -> pd.DataFrame:
+        """Internal read method. Should not be called directly. Use read() instead.
+
+        Overwrite this method to implement custom read logic.
+        The main read() method is replaced when in uhura testing mode."""
+        ...
+
+    @abstractmethod
+    def _write(self, df: pd.DataFrame) -> None:
+        """Internal write method. Should not be called directly. Use write() instead.
+
+        Overwrite this method to implement custom write logic.
+        The main write() method is replaced when in uhura testing mode."""
+        ...
+
+    def read(self) -> pd.DataFrame:
+        """Read the resource."""
+        df = self._read()
+        df = self.get_serde().validate(df)
+        return df
+
+    def write(self, df: pd.DataFrame) -> None:
+        """Write the resource."""
+        df = self.get_serde().validate(df)
+        self._write(df)
+
+    def cache_key(self):
+        """Return the test path."""
+        if self.test_path is None:
+            raise ValueError("No test path set. Please implement fixture_path property or set test_path.")
+        return str(self.test_path)
+
+    @property
+    def serde_class(self) -> Type[BaseSerde]:
+        """Return the serde class. Default is PickleSerde."""
+        return PickleSerde
+
+    def get_serde(self) -> BaseSerde:
+        """Return the serde instance, with baked-in validation."""
+        validations = []
+        if self.pa_schema is not None:
+            # validations.append(create_schema_validator(self.pa_schema))
+            validations.append(self.pa_schema.validate)
+
+        return self.serde_class(validations=validations)
---
+++ dynamicio/io/serde.py
@@ -0,0 +1,105 @@
+"""These are the base serde classes, used for testing & when appropriate for actual IO."""
+import pickle
+from abc import ABC, abstractmethod
+from typing import Callable, Optional, TypeVar
+
+import pandas as pd
+from uhura.serde import Serde
+
+SerdeType = TypeVar("SerdeType")
+
+
+class BaseSerde(ABC, Serde[pd.DataFrame]):
+    def __init__(self, validations: Optional[Callable] = None, **kwargs):
+        self.validations = validations or []
+
+    def read_from_file(self, file) -> pd.DataFrame:
+        df = self._read(file)
+        return self.validate(df)
+
+    @abstractmethod
+    def _read(self, file) -> pd.DataFrame:
+        raise NotImplementedError
+
+    def write_to_file(self, path: str, obj: pd.DataFrame) -> None:
+        return self._write(path, obj)
+
+    @abstractmethod
+    def _write(self, path: str, obj: pd.DataFrame) -> None:
+        raise NotImplementedError
+
+    def validate(self, df: pd.DataFrame):
+        """Validation is done here to avoid double validations in the framework."""
+        for validator in self.validations:
+            validator(df)
+        return df
+
+
+class PickleSerde(BaseSerde):
+    def _read(self, file) -> SerdeType:
+        with open(file, "rb") as infile:
+            return pickle.load(infile)
+
+    def _write(self, file, obj: SerdeType):
+        with open(file, "wb") as outfile:
+            return pickle.dump(obj, outfile)
+
+
+class ParquetSerde(BaseSerde):
+    file_extension = "_"
+
+    def __init__(self, read_kwargs=None, write_kwargs=None, **kwargs):
+        self._read_kwargs = read_kwargs or {}
+        self._write_kwargs = write_kwargs or {}
+        super().__init__(**kwargs)
+
+    def _read(self, file: str) -> pd.DataFrame:
+        return pd.read_parquet(file, **self._read_kwargs)
+
+    def _write(self, file: str, obj: pd.DataFrame):
+        return obj.to_parquet(file, **self._write_kwargs)
+
+
+class HdfSerde(BaseSerde):
+    file_extension = "_"
+
+    def __init__(self, read_kwargs=None, write_kwargs=None, **kwargs):
+        self._read_kwargs = read_kwargs or {}
+        self._write_kwargs = write_kwargs or {}
+        super().__init__(**kwargs)
+
+    def _read(self, file: str) -> pd.DataFrame:
+        raise NotImplementedError
+
+    def _write(self, file: str, obj: pd.DataFrame):
+        raise NotImplementedError
+
+
+class CsvSerde(BaseSerde):
+    file_extension = "_"
+
+    def __init__(self, read_kwargs=None, write_kwargs=None, **kwargs):
+        self._read_kwargs = read_kwargs or {}
+        self._write_kwargs = write_kwargs or {"index": False}
+        super().__init__(**kwargs)
+
+    def _read(self, file: str) -> pd.DataFrame:
+        return pd.read_csv(file, **self._read_kwargs)
+
+    def _write(self, file: str, obj: pd.DataFrame):
+        raise obj.to_csv(file, **self._write_kwargs)
+
+
+class JsonSerde(BaseSerde):
+    file_extension = "_"
+
+    def __init__(self, read_kwargs=None, write_kwargs=None, **kwargs):
+        self._read_kwargs = read_kwargs or {}
+        self._write_kwargs = write_kwargs or {}
+        super().__init__(**kwargs)
+
+    def _read(self, file: str) -> pd.DataFrame:
+        return pd.read_json(file, **self._read_kwargs)
+
+    def _write(self, file: str, obj: pd.DataFrame):
+        raise obj.to_json(file, **self._write_kwargs)
